28eb799a8d857baa350f2bea5af8ed3923f425e9
toke00t <99003274+toke00t@users.noreply.github.com>
2023-02-15 16:04:39 +0100

    q 1-3 done

--- assigment1.py
+++ assigment1.py
@@ -15,6 +15,22 @@ plt.rcParams['savefig.dpi'] = 300
 # Giver skriftstørrelse 12 som standard på plots i stedet for 10 som er default
 plt.rcParams.update({'font.size': 12})
 
+#%% Indstillinger til de forskellige spørgsmål
+
+# if use_wind_shear = False then wind_shear = 0
+# if use_wind_shear = True then wind_shear = 0.2
+use_wind_shear = True
+
+# if use_pitch = True then pitch is changed in time (see assignment description)
+# if use_pitch = False then the pitch is always 0
+use_pitch = True
+
+# Dynamic wake filter
+use_dwf = True
+
+# Dynamic stall
+use_stall = True
+
 #%% Force coeff files
 
 
@@ -64,8 +80,19 @@ omega= 7.229*2*np.pi/60 # rad/s
 # omega = 9.6*2*np.pi/60 # rad/s gammel opgave
 delta_t=0.15 # s
 timerange=1200
-wind_shear=0.2
+# timerange=200
+
+if use_wind_shear:
+    wind_shear = 0.2
+else:
+    wind_shear = 0
+
 V_0=9 # mean windspeed at hub height m/s
+
+# Dynamic wake filter constant
+k_dwf = 0.6
+
+#%% Transformation matrices
 
 a1=np.array([[1,0,0],
           [0,np.cos(theta_yaw),np.sin(theta_yaw)],
@@ -105,6 +132,7 @@ T_arr = np.zeros([timerange])
 P_arr = np.zeros([timerange])
 CT_arr = np.zeros([timerange])
 CP_arr = np.zeros([timerange])
+T_all_arr = np.zeros([B, timerange])
 
 # Blade position: 2D array (blade number, time)
 theta_blade_arr = np.zeros([B,timerange])
@@ -128,9 +156,18 @@ V_rel_z_arr = np.zeros([len(airfoils), B, timerange])
 
 Wy_arr = np.zeros([len(airfoils), B, timerange])
 Wz_arr = np.zeros([len(airfoils), B, timerange])
+Wy_qs_arr = np.zeros([len(airfoils), B, timerange])
+Wz_qs_arr = np.zeros([len(airfoils), B, timerange])
+Wy_int_arr = np.zeros([len(airfoils), B, timerange])
+Wz_int_arr = np.zeros([len(airfoils), B, timerange])
 
 pt_arr = np.zeros([len(airfoils), B, timerange])
 pn_arr = np.zeros([len(airfoils), B, timerange])
+
+fs_arr = np.zeros([len(airfoils), B, timerange])
+cl_arr = np.zeros([len(airfoils), B, timerange])
+
+
 
 
 #%% Loop
@@ -140,11 +177,12 @@ for n in range(1,timerange):
     time_arr[n] = n*delta_t
     
     
-    if 100 <= time_arr[n] <= 150:
-        theta_pitch= np.deg2rad(2)    
-    elif 150 < time_arr[n]:
-        theta_pitch= 0
-        
+    if use_pitch:
+        if 100 <= time_arr[n] <= 150:
+            theta_pitch= np.deg2rad(2)    
+        elif 150 < time_arr[n]:
+            theta_pitch= 0
+            
     
     for i in range(B):
         
@@ -193,22 +231,27 @@ for n in range(1,timerange):
             # hvilket vores relative hastigheder også er
             V_rel_y_arr[k, i, n] = V0y_arr[k, i, n] + Wy_arr[k, i, n-1] - omega * r[k] * np.cos(theta_cone)
             V_rel_z_arr[k, i, n] = V0z_arr[k, i, n] + Wz_arr[k, i, n-1]
-                
-            # if k==17:
-                # print(V_rel_z_arr[k, i, n]/(-V_rel_y_arr[k, i, n]))
-                # print(-V_rel_y_arr[k, i, n])
-            
-            
+
             phi = np.arctan(V_rel_z_arr[k, i, n]/(-V_rel_y_arr[k, i, n]))
-            
             
             aoa_deg = np.rad2deg(phi) - (beta_deg[k] + np.rad2deg(theta_pitch))
             
             # cl, cd, cm skal opdateres til cl, cd, cm, _, _, _ senere
             # Index af tc skal sættes til nummer airfoil
-            cl, cd, cm = force_coeffs_10MW(aoa_deg, tc[k], aoa_tab, cl_stat_tab, cd_stat_tab, cm_stat_tab)
-                                    
+            cl, cd, cm, f_stat, cl_inv, cl_fs = force_coeffs_10MW(aoa_deg, tc[k], aoa_tab, cl_stat_tab, cd_stat_tab, cm_stat_tab, f_stat_tab, cl_inv_tab, cl_fs_tab)
+            
             V_rel_abs = np.sqrt(V_rel_y_arr[k, i, n]**2 + V_rel_z_arr[k, i, n]**2)
+            
+            if use_stall:
+                tau_stall = 4 * c[k] / V_rel_abs
+                
+                fs_arr[k, i, n] = f_stat + (fs_arr[k, i, n-1]-f_stat) * np.exp(-delta_t/tau_stall)
+                
+                cl_arr[k, i, n] = f_stat * cl_inv + (1-fs_arr[k, i, n]) * cl_fs
+            
+            else:
+                cl_arr[k, i, n] = cl
+            
                         
             # V_0 er konstant nu, men skal opdateres til en liste når turbulens tages med
                         
@@ -221,7 +264,7 @@ for n in range(1,timerange):
             
             V_f_W = np.sqrt(V0y_arr[k, i, n]**2 + (V0z_arr[k, i, n] + f_g * Wz_arr[k, i, n-1])**2)
             
-            l = 0.5 * rho * V_rel_abs**2 * c[k] * cl
+            l = 0.5 * rho * V_rel_abs**2 * c[k] * cl_arr[k, i, n]
             d = 0.5 * rho * V_rel_abs**2 * c[k] * cd
            
             if k==len(airfoils)-1:
@@ -243,23 +286,40 @@ for n in range(1,timerange):
             else:
                 F = (2/np.pi) * np.arccos(np.exp(-(B/2) * ((R-r[k])/(r[k] * np.sin(abs(phi))))))
             
-            W_z_qs = (-B * l * np.cos(phi))/(4 * np.pi * rho * r[k] * F * V_f_W)
-            W_y_qs = (-B * l * np.sin(phi))/(4 * np.pi * rho * r[k] * F * V_f_W)
+            Wz_qs_arr[k, i, n] = (-B * l * np.cos(phi))/(4 * np.pi * rho * r[k] * F * V_f_W)
+            Wy_qs_arr[k, i, n] = (-B * l * np.sin(phi))/(4 * np.pi * rho * r[k] * F * V_f_W)
             
             
             # Når vi ikke har timefilter på endnu
-            
-            Wz_arr[k, i, n] = W_z_qs
-            Wy_arr[k, i, n] = W_y_qs
+            if use_dwf:
+                tau_1 = 1.1/(1-1.3*a)*R/V_0
+                tau_2 = (0.39 - 0.26 * (r[k]/R)**2)*tau_1
+                
+                Hy_dwf = Wy_qs_arr[k, i, n] + k_dwf * tau_1 * (Wy_qs_arr[k, i, n] - Wy_qs_arr[k, i, n-1])/delta_t
+                Hz_dwf = Wz_qs_arr[k, i, n] + k_dwf * tau_1 * (Wz_qs_arr[k, i, n] - Wz_qs_arr[k, i, n-1])/delta_t
+                
+                Wy_int_arr[k, i, n] = Hy_dwf + (Wy_int_arr[k, i, n-1] - Hy_dwf)*np.exp(-delta_t/tau_1)
+                Wz_int_arr[k, i, n] = Hz_dwf + (Wz_int_arr[k, i, n-1] - Hz_dwf)*np.exp(-delta_t/tau_1)
+                
+                Wy_arr[k, i, n] = Wy_int_arr[k, i, n] + (Wy_arr[k, i, n-1] - Wy_int_arr[k, i, n])*np.exp(-delta_t/tau_2)
+                Wz_arr[k, i, n] = Wz_int_arr[k, i, n] + (Wz_arr[k, i, n-1] - Wz_int_arr[k, i, n])*np.exp(-delta_t/tau_2)
+                
+            else:
+                Wz_arr[k, i, n] = Wz_qs_arr[k, i, n]
+                Wy_arr[k, i, n] = Wy_qs_arr[k, i, n]
             
        
     # OBS: i stedet for at gange op til 3 blades så prøv at summér med de faktiske værdier
-    M_r = B * np.trapz(pt_arr[:,0,n]*r,r)
-    # M_r = np.trapz(np.sum(pt_arr,axis=1)[:,n]*r,r)
+    # M_r = B * np.trapz(pt_arr[:,0,n]*r,r)
+    M_r = np.trapz(np.sum(pt_arr,axis=1)[:,n]*r,r)
     
     P_arr[n] = omega*M_r
-    T = B*np.trapz(pn_arr[:,0,n],r)
-    # T = np.trapz(np.sum(pn_arr,axis=1)[:,n],r)
+    # T = B*np.trapz(pn_arr[:,0,n],r)
+    T_all_arr[0,n] = np.trapz(pn_arr[:,0,n],r)
+    T_all_arr[1,n] = np.trapz(pn_arr[:,1,n],r)
+    T_all_arr[2,n] = np.trapz(pn_arr[:,2,n],r)
+    
+    T = np.trapz(np.sum(pn_arr,axis=1)[:,n],r)
     T_arr[n] = T
     
     
@@ -282,7 +342,8 @@ plt.show()
 blade_element = 17
 plt.figure()
 plt.grid()
-plt.title('Blade position, airfoil {} (1-based indexing)'.format(blade_element+1))
+# plt.title('Blade position, airfoil {} (1-based indexing)'.format(blade_element+1))
+plt.title('Blade position for the last blade element')
 # For de tre vinger
 for i in range(B):
     plt.plot(x1_arr[blade_element,i,1:], y1_arr[blade_element,i,1:],linewidth=7-3*i,label='Blade {}'.format(i+1))
@@ -301,13 +362,15 @@ plt.show()
 blade_element = 17
 plt.figure()
 plt.grid()
-plt.title('System 4 x-position for blade element {} (1-based indexing)'.format(blade_element+1))
+# plt.title('System 4 x-position for blade element {} (1-based indexing)'.format(blade_element+1))
+plt.title('System 4 x-position for the last blade element')
 # For de tre vinger
 for i in range(B):
     plt.plot(time_arr, x1_arr[blade_element,i,:],label='Blade {}'.format(i+1))
 # Tilføjer r
-plt.plot([max(time_arr)*2/3,max(time_arr)*2/3],[H-r[blade_element],H+r[blade_element]],'--',label='r = {:.2f} m'.format(r[blade_element]))
+plt.plot([max(time_arr)*2/3,max(time_arr)*2/3],[H-r[blade_element],H+r[blade_element]],'--',label='2$\cdot$r = {:.2f} m'.format(2*r[blade_element]))
 plt.xlim([0,max(time_arr)])
+plt.ylim(0)
 plt.xlabel('Time [s]')
 plt.ylabel('x [m]')
 plt.legend()
@@ -353,8 +416,8 @@ plt.grid()
 plt.title('Load distribution for blade {} (1-based indexing)'.format(blade_number+1))
 plt.plot(r,pn_arr[:, blade_number, -1],label='$p_n$',marker='o')
 plt.plot(r,pt_arr[:, blade_number, -1],label='$p_t$',marker='x')
-plt.plot(r,martin_py[:,0],label='Martins $p_t$',marker='*')
-plt.plot(r,martin_pz[:,0]*1000,label='Martins $p_n$',marker='|')
+# plt.plot(r,martin_py[:,0],label='Martins $p_t$',marker='*')
+# plt.plot(r,martin_pz[:,0]*1000,label='Martins $p_n$',marker='|')
 plt.xlim(0)
 plt.xlabel('r [m]')
 plt.ylabel('p [N]')
@@ -373,29 +436,21 @@ plt.ylabel('p [N]')
 plt.legend()
 plt.show()
 
+#%% 
 
-#%% Creating figure with subplots of C_T and C_P
-# fig, ax1 = plt.subplots()
+plt.figure()
+plt.grid()
+plt.title('Thrust')
+for i in range(B):
+    plt.plot(time_arr, T_all_arr[i,:]/10**6,label='Blade {}'.format(i+1))
+plt.plot(time_arr, T_arr/(10**6),label = 'Total')
+plt.xlabel('Time [s]')
+plt.ylabel('Thrust [MN]')
+plt.xlim([0,time_arr[-1]])
+plt.legend()
+plt.show()
 
-# fig.suptitle('Thrust and power')
-# color = 'tab:orange'
-# ax1.grid()
-# ax1.set_xlabel('Time [s]')
-# ax1.set_ylabel('$C_T$ [-]', color=color)
-# ax1.plot(time_arr, CT_arr, color=color)
-# ax1.tick_params(axis='y', labelcolor=color)
 
-# ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
-
-# color = 'tab:blue'
-# ax2.set_ylabel('$C_P$ [-]', color=color)  # we already handled the x-label with ax1
-# ax2.plot(time_arr, CP_arr, color=color)
-# # Man skal af en eller anden grund have denne her linje med for at de to y-akser bliver aligned
-# ax2.set_yticks(np.linspace(ax2.get_yticks()[0],ax2.get_yticks()[-1],len(ax1.get_yticks())))
-# ax2.tick_params(axis='y', labelcolor=color)
-
-# fig.tight_layout()  # otherwise the right y-label is slightly clipped
-# plt.show()
 
 
 
--- interpolation.py
+++ interpolation.py
@@ -8,7 +8,7 @@ import numpy as np
 
 #TEST OF INTERPOLATION ROUTINE. COMPARE TO INTERP1 IN MATLAB
 
-def force_coeffs_10MW(angle_of_attack,thick,aoa, cl_tab, cd_tab, cm_tab): #Creating a function which takes the angle of attack and the section thickness:
+def force_coeffs_10MW(angle_of_attack,thick,aoa, cl_tab, cd_tab, cm_tab, f_stat_tab, cl_inv_tab, cl_fs_tab): #Creating a function which takes the angle of attack and the section thickness:
     
     
     # angle_of_attack=5
@@ -32,6 +32,9 @@ def force_coeffs_10MW(angle_of_attack,thick,aoa, cl_tab, cd_tab, cm_tab): #Creat
     cl_aoa=np.zeros([1,len(files)])
     cd_aoa=np.zeros([1,len(files)])
     cm_aoa=np.zeros([1,len(files)])
+    f_stat_aoa = np.zeros([1,len(files)])
+    cl_inv_aoa = np.zeros([1,len(files)])
+    cl_fs_aoa = np.zeros([1,len(files)])
     
     
     #Interpolate to current angle of attack:
@@ -39,11 +42,17 @@ def force_coeffs_10MW(angle_of_attack,thick,aoa, cl_tab, cd_tab, cm_tab): #Creat
         cl_aoa[0,i]=np.interp(angle_of_attack,aoa,cl_tab[:,i])
         cd_aoa[0,i]=np.interp(angle_of_attack,aoa,cd_tab[:,i])
         cm_aoa[0,i]=np.interp(angle_of_attack,aoa,cm_tab[:,i])
+        f_stat_aoa[0,i]=np.interp(angle_of_attack,aoa,f_stat_tab[:,i])
+        cl_inv_aoa[0,i]=np.interp(angle_of_attack,aoa,cl_inv_tab[:,i])
+        cl_fs_aoa[0,i]=np.interp(angle_of_attack,aoa,cl_fs_tab[:,i])
     
     #Interpolate to current thickness:
-    cl=np.interp(thick,thick_prof,cl_aoa[0,:])
-    cd=np.interp(thick,thick_prof,cd_aoa[0,:])
-    cm=np.interp(thick,thick_prof,cm_aoa[0,:])
+    cl = np.interp(thick,thick_prof,cl_aoa[0,:])
+    cd = np.interp(thick,thick_prof,cd_aoa[0,:])
+    cm = np.interp(thick,thick_prof,cm_aoa[0,:])
+    f_stat = np.interp(thick,thick_prof,f_stat_aoa[0,:])
+    cl_inv = np.interp(thick,thick_prof,cl_inv_aoa[0,:])
+    cl_fs = np.interp(thick,thick_prof,cl_fs_aoa[0,:])
 
-    return cl, cd, cm
+    return cl, cd, cm, f_stat, cl_inv, cl_fs
 
