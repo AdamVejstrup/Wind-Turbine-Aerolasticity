29d6fb1a5b9a2a52d6ccb3f4668f342aa5428934
toke00t <99003274+toke00t@users.noreply.github.com>
2023-04-12 15:39:07 +0200

    Update newmark_exercise.py

--- newmark_exercise.py
+++ newmark_exercise.py
@@ -8,6 +8,9 @@ Created on Wed Mar 29 10:52:17 2023
 import numpy as np
 import matplotlib.pyplot as plt
 
+linear_newmark = False
+non_linear_newmark = True
+
 L = 2 # [m]
 m1 = 1 # [kg] mass of cart
 m2 = 0.5 # [kg/m] mass of beam
@@ -18,10 +21,17 @@ im = (m2 * L**3) / 3 # Coordinate [1, 1] of mass matrix
 g = 9.81 # [m/s**2]
 
 # Given parameters
-beta = 0.25
-gamma = 0.50
+if linear_newmark:
+    beta = 0.25
+    gamma = 0.50
+elif non_linear_newmark:
+    beta = 0.27
+    gamma = 0.51
+    eps = 10**(-2)
+else:
+    raise ValueError('Please choose between linear or non-linear newmark')
 
-h = 0.0001 # Timestep
+h = 0.001 # Timestep
 
 tstart = 0 # Start time 
 tend = 50 # End time
@@ -59,39 +69,86 @@ ddx = np.zeros(x.shape)
 ddx[:, 0] = np.linalg.inv(M_star) @ (gf[:, 0] - C@dx[:, 0] - K@x[:, 0])
 
 
+if linear_newmark:
+    for n in range(1, len(time)):
+        # Step 3: Prediction step
+        
+        dx_up = dx[:, n-1] + (1-gamma)*h * ddx[:, n-1]
+        
+        x_up = x[:, n-1] + h*dx[:, n-1] + (0.5-beta)*h**2*ddx[:, n-1]
+        
+        # Step 4: Correction step
+        
+        M_up = np.array([[m2*L + m1, -sm*np.sin(x_up[1])],
+                    [-sm*np.sin(x_up[1]), im]])
+        
+        gf_up = np.array([dx_up[1]**2 * np.cos(x_up[1]) * sm,
+                        g*sm * np.cos(x_up[1])])
+        
+        M_star_up = M_up + gamma*h*C + beta*(h**2)*K
+        
+        ddx_correct = np.linalg.inv(M_star_up) @ (gf_up - (C @ dx[:, n-1]) - (K @ x[:, n-1]))
+        
+        dx_correct = dx_up + gamma*h*ddx_correct
+        
+        x_correct = x_up + beta*h**2*ddx_correct
+        
+        # Updating position, velocity and acceleration
+        x[:, n] = x_correct
+        dx[:, n] = dx_correct
+        ddx[:, n] = ddx_correct
 
-for n in range(1, len(time)):
-    # Step 3: Prediction step
-    
-    dx_up = dx[:, n-1] + (1-gamma)*h * ddx[:, n-1]
-    # dx_up = dx[:, n] + (1-gamma)*h * ddx[:, n]
-    
-    x_up = x[:, n-1] + h*dx[:, n-1] + (0.5-beta)*h**2*ddx[:, n-1]
-    # x_up = x[:, n] + h*dx[:, n] + (0.5-beta)*h**2*ddx[:, n]
-    
-    # Step 4: Correction step
-    
-    M_up = np.array([[m2*L + m1, -sm*np.sin(x_up[1])],
-                  [-sm*np.sin(x_up[1]), im]])
-    
-    gf_up = np.array([dx_up[1]**2 * np.cos(x_up[1]) * sm,
-                      g*sm * np.cos(x_up[1])])
-    
-    M_star_up = M_up + gamma*h*C + beta*(h**2)*K
-    
-    ddx_correct = np.linalg.inv(M_star_up) @ (gf_up - (C @ dx[:, n-1]) - (K @ x[:, n-1]))
-    # ddx_correct = np.linalg.inv(M_star_up) @ (gf_up - C@dx[:, n] - K@x[:, n])
-    
-    dx_correct = dx_up + gamma*h*ddx_correct
-    
-    x_correct = x_up + beta*h**2*ddx_correct
-    
-    # Updating position, velocity and acceleration
-    x[:, n] = x_correct
-    dx[:, n] = dx_correct
-    ddx[:, n] = ddx_correct
+else:
+    for n in range(1, len(time)):
+        
+        # Step 2: Predictions of position, velocity and acceleration
+        x_up = x[:, n-1] + h*dx[:, n-1] + 0.5* h**2 *ddx[:, n-1]
+        dx_up = dx[:, n-1] + h*ddx[:, n-1]
+        ddx_up = ddx[:, n-1]
+        
+        # Step 3: Residual
+        # M_up = np.array([[m2*L + m1, -sm*np.sin(x_up[1])],
+        #             [-sm*np.sin(x_up[1]), im]])
+        
+        # gf_up = np.array([dx_up[1]**2 * np.cos(x_up[1]) * sm,
+        #                 g*sm * np.cos(x_up[1])])
+        counter = 0
+        r = np.array([1, 1])
+        
+        while (r[0] >= eps) and (r[1] >= eps) or (counter < 600):
+            M_up = np.array([[m2*L + m1, -sm*np.sin(x_up[1])],
+                        [-sm*np.sin(x_up[1]), im]])
+            
+            gf_up = np.array([dx_up[1]**2 * np.cos(x_up[1]) * sm,
+                            g*sm * np.cos(x_up[1])])
+            
+            # r = gf_up - M_up @ ddx_up[:, n] - C @ dx_up[:, n] - K @ x_up[:, n]
+            r = gf_up - M_up @ ddx_up - C @ dx_up - K @ x_up
+            
+            K_star = K + (gamma*h) / (beta * h) * C + 1/(beta * h**2) * M
+            
+            delta_x = np.linalg.inv(K_star) @ r
+            
+            # New x_up that is only used in the while loop
+            # x_up_while = x_up + delta_x
+            # dx_up_while = dx_up + gamma / (beta*h) * delta_x
+            # ddx_up_while = ddx_up + (1 / beta*h**2) * delta_x
+            
+            x_up = x_up + delta_x
+            dx_up = dx_up + gamma / (beta*h) * delta_x
+            ddx_up = ddx_up + (1 / beta*h**2) * delta_x
+            
+            # Update counter
+            counter = counter + 1
+        if n <= 100:
+            print(counter)
+        x[:, n] = x_up
+        dx[:, n] = dx_up
+        ddx[:, n] = ddx_up
+            
 
-# %% Plotting the results
+# Plotting the results
+
 plt.figure()
 plt.grid()
 plt.title(f'Cart position and beam angular positon, timestep = {h} s')
